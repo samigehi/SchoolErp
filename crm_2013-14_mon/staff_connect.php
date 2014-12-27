36b8115859632b1bd662ddfe68b8cba8ca11d8e9
Sekhar <sekhar@thevalleyschool.info>
2014-12-27 08:54:49 +0530

    fixes on DB

--- crm_2013-14_mon/staff_connect.php
+++ crm_2013-14_mon/staff_connect.php
@@ -2,7 +2,7 @@
 $hostname='localhost'; //// specify host, i.e. 'localhost'
 $user='thevall7_erp'; //// specify username
 $pass='thevalleyschool123'; //// specify password
-$dbase='thevall7_ss_staff'; //// specify database name
+$dbase='thevall7_erp_ss_staff'; //// specify database name
 $connection = mysql_connect("$hostname" , "$user" , "$pass") 
 or die ("Can't connect to MySQL");
 $db = mysql_select_db($dbase , $connection) or die ("Can't select database.");
